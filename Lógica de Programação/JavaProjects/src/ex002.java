8850d7d19bcee00f543e37451193eb76e70c4e5d
Maria Fernanda Vaz <vazfernandam@gmail.com>
2020-05-15 18:23:18 -0300

    Java ex2

---
+++ Lógica de Programação/JavaProjects/src/ex002.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ex002 {
+    public static void main(String args[]){
+        //exemplo do comando for
+        Scanner teclado = new Scanner(System.in);
+        int numero;
+        for(int i=1; i<=5; i++){
+            System.out.println("Número: ");
+            numero = teclado.nextInt();
+            if(numero % 2 == 0){
+                System.out.println("O número é par!");
+            }else{
+                System.out.println("O número é ímpar!");
+            }
+        }
+    }
+}
