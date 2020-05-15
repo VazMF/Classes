f4184f1b1ebd6d140a70eef5370224f48b7f9583
Maria Fernanda Vaz <vazfernandam@gmail.com>
2020-05-15 17:59:01 -0300

    Java ex1

---
+++ Lógica de Programação/JavaProjects/src/ex001.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ex001 {
+    public static void main(String args[]){
+        double valor1, valor2;
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro valor: ");
+        valor1 = teclado.nextDouble();
+
+        System.out.println("Informe o segundo valor: ");
+        valor2 = teclado.nextDouble();
+
+        if(valor1 > valor2){
+            System.out.println("O primeiro valor é maior.");
+        }else{
+            System.out.println("O segundo valor é maior.");
+        }
+    }
+}
