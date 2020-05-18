9ed0b6bb7a091d145f13d4e4107df28dc6cc0649
Maria Fernanda Vaz <vazfernandam@gmail.com>
2020-05-17 22:38:29 -0300

    java

---
+++ Lógica de Programação/JavaProjects/src/exWhile.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+//ex para demonstração do while
+
+public class exWhile {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner((System.in));
+        int numero;
+        System.out.print("Numero: ");
+        numero = teclado.nextInt();
+        while(numero>100) {
+            System.out.println(numero);
+            numero--;
+        }
+        System.out.println("Fim do while.");
+    }
+}
---
+++ Lógica de Programação/JavaProjects/src/exWhile2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class exWhile2 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner((System.in));
+        int numero;
+        System.out.print("Numero: ");
+        numero = teclado.nextInt();
+        do{
+            System.out.println(numero);
+            numero = teclado.nextInt();
+        }while(numero!=44);
+        System.out.println("Fim do while.");
+    }
+}
+
